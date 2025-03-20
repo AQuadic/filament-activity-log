5cade228b0d6064679731fa3d0aabc6d1ea247a6
Anxis <mohamedamrfouad19@gmail.com>
2025-03-20 11:18:37 +0200

    config key change

--- config/filament-activity-log.php
+++ config/filament-activity-log.php
@@ -4,9 +4,9 @@ use Noxo\FilamentActivityLog\ResourceLogger\Types\BooleanEnum;
 
 return [
     'loggers' => [
-        [
+        'main'=>[
             'directory' => app_path('Filament/Loggers'),
-        'namespace' => 'App\\Filament\\Loggers',
+            'namespace' => 'App\\Filament\\Loggers',
         ],
     ],
 
